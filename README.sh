8ae34a63b3af693c740c25fc819f90f4c7534948
sekrit-twc <noreply@example.com>
2016-06-16 09:38:49 -0700

    Add build system.

---
+++ README.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+set -x
+
+rm -f *.o *.a *.so a.out
+
+if [ "x$1" = "xclean" ]; then exit 0; fi
+
+CXXFLAGS="-std=c++11 -Wall -Wextra -Wno-missing-field-initializers -O2"
+CFLAGS="-std=c89 -Wall -Wextra -Wno-missing-field-initializers -O2"
+
+c++ ${CXXFLAGS} -c -o p2p.o p2p.cpp
+c++ ${CXXFLAGS} -c -o v210.o v210.cpp
+ar rc libp2p.a p2p.o v210.o
+
+cc ${CFLAGS} -L. main.c -lp2p
