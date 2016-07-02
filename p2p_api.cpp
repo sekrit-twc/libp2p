e1b0b8392b11d5dfbd9e3cd993436e51525310a2
sekrit-twc <noreply@example.com>
2016-07-02 13:48:33 -0700

    Fix pack_plane/unpack_plane for 10-bit NV-like formats.

--- p2p_api.cpp
+++ p2p_api.cpp
@@ -14,18 +14,20 @@ struct packing_traits {
 	enum p2p_packing packing;
 	p2p_unpack_func unpack;
 	p2p_pack_func pack;
+	bool native_endian;
 	unsigned char subsample_w;
 	unsigned char subsample_h;
-	unsigned char bytes_per_sample;	// Only used to copy luma plane for NV12.
 	bool is_nv;
+	unsigned char bytes_per_sample;	// Only used to copy luma plane for NV12.
+	unsigned char nv_shift; // Extra LSB to shift away for MS P010/P210, etc.
 };
 
 #define CASE(x, ...) \
 	{ p2p_##x, &p2p::packed_to_planar<p2p::packed_##x>::unpack, &p2p::planar_to_packed<p2p::packed_##x>::pack, ##__VA_ARGS__}
 #define CASE2(x, ...) \
-	CASE(x##_be, ##__VA_ARGS__), \
-	CASE(x##_le, ##__VA_ARGS__), \
-	CASE(x, ##__VA_ARGS__)
+	CASE(x##_be, std::is_same<p2p::native_endian_t, p2p::big_endian_t>::value, ##__VA_ARGS__), \
+	CASE(x##_le, std::is_same<p2p::native_endian_t, p2p::little_endian_t>::value, ##__VA_ARGS__), \
+	CASE(x, true, ##__VA_ARGS__)
 const packing_traits traits_table[] = {
 	CASE2(rgb24, 0, 0),
 	CASE2(argb32, 0, 0),
@@ -35,18 +37,18 @@ const packing_traits traits_table[] = {
 	CASE2(rgb30, 0, 0),
 	CASE2(y410, 0, 0),
 	CASE2(y416, 0, 0),
-	CASE(yuy2, 1, 0),
-	CASE(uyvy, 1, 0),
+	CASE(yuy2, true, 1, 0),
+	CASE(uyvy, true, 1, 0),
 	CASE2(y210, 1, 0),
 	CASE2(y216, 1, 0),
 	CASE2(v210, 1, 0),
 	CASE2(v216, 1, 0),
 	// For NV12-like formats, bytes_per_pel is the number of bytes in a UV pair.
-	CASE2(nv12, 1, 1, 1, true),
-	CASE2(p010, 1, 1, 2, true),
-	CASE2(p016, 1, 1, 2, true),
-	CASE2(p210, 1, 0, 2, true),
-	CASE2(p216, 1, 0, 2, true),
+	CASE2(nv12, 1, 1, true, 1),
+	CASE2(p010, 1, 1, true, 2, 6),
+	CASE2(p016, 1, 1, true, 2),
+	CASE2(p210, 1, 0, true, 2, 6),
+	CASE2(p216, 1, 0, true, 2),
 };
 #undef CASE2
 #undef CASE
@@ -68,11 +70,45 @@ T *increment_ptr(T *ptr, ptrdiff_t n)
 	return (T *)((const unsigned char *)ptr + n);
 }
 
-void copy_plane(const void *src, void *dst, ptrdiff_t src_stride, ptrdiff_t dst_stride,
-                unsigned linesize, unsigned height)
+void copy_plane_fast(const void *src, void *dst, ptrdiff_t src_stride, ptrdiff_t dst_stride,
+                     unsigned linesize, unsigned height)
 {
 	for (unsigned i = 0; i < height; ++i) {
 		memcpy(dst, src, linesize);
+
+		src = increment_ptr(src, src_stride);
+		dst = increment_ptr(dst, dst_stride);
+	}
+}
+
+void unpack_nv16_plane(const void *src, void *dst, ptrdiff_t src_stride, ptrdiff_t dst_stride,
+                       const packing_traits &traits, unsigned width, unsigned height)
+{
+	assert(traits.bytes_per_sample == 1);
+
+	for (unsigned i = 0; i < height; ++i) {
+		std::transform(static_cast<const uint16_t *>(src), static_cast<const uint16_t *>(src) + width, static_cast<uint16_t *>(dst), [=](uint16_t x)
+		{
+			x = traits.native_endian ? x : (x >> 8) | (x << 8);
+			return x >> traits.nv_shift;
+		});
+
+		src = increment_ptr(src, src_stride);
+		dst = increment_ptr(dst, dst_stride);
+	}
+}
+
+void pack_nv16_plane(const void *src, void *dst, ptrdiff_t src_stride, ptrdiff_t dst_stride,
+                     const packing_traits &traits, unsigned width, unsigned height)
+{
+	assert(traits.bytes_per_sample == 1);
+
+	for (unsigned i = 0; i < height; ++i) {
+		std::transform(static_cast<const uint16_t *>(src), static_cast<const uint16_t *>(src) + width, static_cast<uint16_t *>(dst), [=](uint16_t x)
+		{
+			x = x << traits.nv_shift;
+			return traits.native_endian ? x : (x >> 8) | (x << 8);
+		});
 
 		src = increment_ptr(src, src_stride);
 		dst = increment_ptr(dst, dst_stride);
@@ -116,8 +152,12 @@ void p2p_unpack_frame(const struct p2p_buffer_param *param, unsigned long flags)
 	}
 
 	if (traits.is_nv && !(flags & P2P_SKIP_UNPACKED_PLANES) && param->src[0] && param->dst[0]) {
-		copy_plane(param->src[0], param->dst[0], param->src_stride[0], param->dst_stride[0],
-		           traits.bytes_per_sample * param->width, param->height);
+		if ((traits.bytes_per_sample == 1 || traits.native_endian) && !traits.nv_shift) {
+			copy_plane_fast(param->src[0], param->dst[0], param->src_stride[0], param->dst_stride[0],
+			                traits.bytes_per_sample * param->width, param->height);
+		} else {
+			unpack_nv16_plane(param->src[0], param->dst[0], param->src_stride[0], param->dst_stride[0], traits, param->width, param->height);
+		}
 	}
 }
 
@@ -145,7 +185,11 @@ void p2p_pack_frame(const struct p2p_buffer_param *param, unsigned long flags)
 	}
 
 	if (traits.is_nv && !(flags & P2P_SKIP_UNPACKED_PLANES) && param->src[0] && param->dst[0]) {
-		copy_plane(param->src[0], param->dst[0], param->src_stride[0], param->dst_stride[0],
-		           traits.bytes_per_sample * param->width, param->height);
+		if ((traits.bytes_per_sample == 1 || traits.native_endian) && !traits.nv_shift) {
+			copy_plane_fast(param->src[0], param->dst[0], param->src_stride[0], param->dst_stride[0],
+			                traits.bytes_per_sample * param->width, param->height);
+		} else {
+			pack_nv16_plane(param->src[0], param->dst[0], param->src_stride[0], param->dst_stride[0], traits, param->width, param->height);
+		}
 	}
 }
